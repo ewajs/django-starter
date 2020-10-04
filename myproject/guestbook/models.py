ef38391bd11e4b7815140c9d17b84096244f007a
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2020-10-04 19:29:34 -0300

    Added better representation for the Comment model (see admin page)

--- myproject/guestbook/models.py
+++ myproject/guestbook/models.py
@@ -6,3 +6,7 @@ class Comment(models.Model):
     name = models.CharField(max_length=20)
     comment = models.TextField()
     date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        # id is provided by models.Model
+        return f"<Name: {self.name}, ID: {self.id}>"
